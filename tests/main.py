b61744b539067426c77a9bea1d2387ef56cd1cf0
Jason Fan <fkt8356@gmail.com>
2024-12-05 09:58:51 -0500

    Add test to make sure the main metrics class works

---
+++ tests/main.py
@@ -0,0 +1,8 @@
+from test_metrics import run_test_metrics
+
+def main():
+    """ Runs generation and analysis steps back-to-back. """
+    run_test_metrics()
+
+if __name__ == "__main__":
+    main()
